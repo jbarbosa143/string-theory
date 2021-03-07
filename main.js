f19b8923ae431094c979cf06a45c7b1ca5143bb9
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-03-07 13:13:12 -0600

    skipped camel didnt fully grasph

--- main.js
+++ main.js
@@ -1,14 +1,164 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function xify(str){
+  var newStr = "";
+  for(i =0; i < str.length; i++){
+    newStr = newStr.concat('x');
+  }
+  return newStr;
+}
+// =========================
+function yellingChars(str){
+  var newStr = "";
+  // create a loop
+  for(i = 0; i < str.length; i++){
+    // now for ea letter in the string add !
+    newStr += str[i] + "!";
+  }
+  return newStr;
+}
+// =========================
+function indexedChars(str){
+  newStr= "";
+  for(i = 0; i < str.length; i++){
+    newStr += i + str[i];
+  }
+  return newStr;
+}
+// ==========================
+function numberedChars(str){
+  newStr= "";
+  for(i =0; i < str.length; i++){
+    newStr += `(${i +1})` + str[i];
+  }
+  return newStr;
+}
+// ===========================
+function exclaim(str){
+  var newStr = "";
+  for(i =0; i < str.length; i++){
+    if(str[i] === "?"  || str[i] === "."){
+      newStr += "!";
+    }else{
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
+// ===============================
+function repeatIt(str,num){
+  var newStr = "";
+  for(i = 0; i < num; i++){
+    newStr += str;
+  }
+  return newStr;
+}
+// ==============================
+function truncate(str){
+  newStr = "";
+  if(str.length > 18){
+    for(i = 0; i < 15; i++){
+      newStr+= str[i];
+    }
+    newStr;
+    newStr +="...";
+    return newStr;
+  }else{
+    return str;
+  }
+}
+// =============================
+function ciEmailify(str){
+  var email = "";
+  for(i = 0; i < str.length; i++){
+    if(str[i] === " "){
+      email += ".";
+    }else{
+      email += str[i];    }
+  }
+  return email.toLowerCase() + "@codeimmersives.com";
+} 
+// =============================
+function reverse(str){
+  reStr ="";
+  for(i = str.length - 1; i >= 0;i--){
+    reStr += str[i];
+  }
+  return reStr;
+}
+// =========================
+function onlyVowels(str){
+  var count = "";
+  for(i =0; i < str.length; i++){
+    if( str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' || str[i] === 'A' || str[i] === 'E' || str[i] === 'I' || str[i] === 'O' || str[i] === 'U'){
+      count += str[i];
+    }
+  }
+  return count;
+}
+// =========================
+// function crazyCase(str){
+// var czyStr ="";
+// var count = 0;
+// for(i = 0; i < str.length; i++);
+// if(str[i] !== " "){
+//   if(count % 2 === 0){
 
+//   }
+// }
+// }
 
+// ========================
+function titleCase(str){
+  newStr = "";
+  for(i = 0; i<str.length; i++){
+    if(i ===0 || str[i - 1] === " " && i >= 1){
+      newStr += str[i].toUpperCase();
+    }else {
+      newStr += str[i].toLowerCase();
+    }
+  }
+  return newStr;
+}
 
-
-
-
-
-
+// ==============================
+function camelCase(str){
+  var cam1 ="";
+  var cam2 = "";
+  for(i = 0; i < str.length; i ++){
+    if(str[i-1] === " " && i > 1){
+      cam1 += str[i].toUpperCase();
+    }else{
+      cam1 += str[i].toLowerCase();
+    }
+  }
+  for(letter of cam1){
+    if(letter !==" "){
+      cam2 += letter;
+    }
+  }
+  return cam2;
+}
+// ===========================
+function crazyCase2ReturnOfCrazyCase(str){
+  var crzyStr = "";
+  var count = 0;
+  for(i =0; i < str.length; i++){
+    if(str[i] !== " "){
+      if(count % 2 === 0){
+        crzyStr += str[i].toLowerCase();
+        count += 1;
+      }else{
+        crzyStr += str[i].toUpperCase();
+        count += 1;
+      }
+    }else{
+      crzyStr += ' ';
+    }
+  }
+  return crzyStr;
+}
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
